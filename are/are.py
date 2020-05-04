3f890ee3251421883f911d527449a07175022917
Andrei Lapets <a@lapets.io>
2020-05-03 22:45:45 -0400

    Base case constructors and embeddable representation method.

--- are/are.py
+++ are/are.py
@@ -5,6 +5,7 @@ mathematical objects.
 """
 
 from __future__ import annotations
+import random
 import doctest
 
 class operation(str):
@@ -13,10 +14,20 @@ class operation(str):
     '''
     pass
 
+op = operation
+
 class are(dict):
     '''
     Abstract data structure for a regular expression.
     '''
+
+    @staticmethod
+    def emp() -> are:
+        return are({operation('emp'): []})
+
+    @staticmethod
+    def lit(value = '') -> are:
+        return are({operation('lit'): [value]})
 
     def star(self: are) -> are:
         return are({operation('star'): [self]})
@@ -29,6 +40,24 @@ class are(dict):
 
     def __and__(self: are, other: are) -> are:
         return are({operation('and'): [self, other]})
+
+    def embedded(self):
+        o = list(self.keys())[0]
+        if o == 'emp':
+            return 'emp()'
+        elif o == 'lit':
+            return 'lit(' + str(list(self.values())[0][0]) + ')'
+        elif o == 'star':
+            return '(' + self[o][0].embedded() + ').star()'
+        elif o == 'con':
+            return '(' + (" + ".join(a.embedded() for a in self[o])) + ')'
+        elif o == 'alt':
+            return '(' + (" | ".join(a.embedded() for a in self[o])) + ')'
+        elif o == 'and':
+            return '(' + (" & ".join(a.embedded() for a in self[o])) + ')'
+
+emp = are.emp
+lit = are.lit
 
 if __name__ == "__main__":
     doctest.testmod()
