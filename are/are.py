44549a66efb39c77db4c8d5a1c2639a085449918
Andrei Lapets <a@lapets.io>
2020-05-05 23:59:19 -0400

    Generate strings of all lengths if no length is supplied.

--- are/are.py
+++ are/are.py
@@ -5,6 +5,7 @@ mathematical objects.
 """
 
 from __future__ import annotations
+import itertools
 import random
 import doctest
 
@@ -220,36 +221,41 @@ class are(dict):
             else:
                 return False
 
-    def strings(self, length):
-        o = list(self.keys())[0]
-        vs = list(self.values())[0]
-        if o == 'emp':
-            pass # No strings accepted.
-        elif o == 'lit':
-            s = list(self.values())[0][0]
-            if length == len(s):
-                yield s
-        elif o == 'star':
-            tmp = are.lit('')
-            for i in range(0, length):
-                for s in tmp.strings(length):
+    def strings(self, length = None):
+        if length is None:
+            for length in itertools.count():
+                for s in self.strings(length):
                     yield s
-                tmp = tmp + vs[0]
-        elif o == 'con':
-            for i in range(0,length):
-                for s1 in vs[0].strings(i):
-                    for s2 in vs[1].strings(length - i):
-                        yield s1 + s2
-        elif o == 'alt':
-            for s in vs[0].strings(length):
-                yield s
-            for s in vs[1].strings(length):
-                if not (s in vs[0]):
+        else:
+            o = list(self.keys())[0]
+            vs = list(self.values())[0]
+            if o == 'emp':
+                pass # No strings accepted.
+            elif o == 'lit':
+                s = list(self.values())[0][0]
+                if length == len(s):
                     yield s
-        elif o == 'and':
-            for s in vs[0].strings(length):
-                if s in vs[1]:
+            elif o == 'star':
+                tmp = are.lit('')
+                for i in range(0, length):
+                    for s in tmp.strings(length):
+                        yield s
+                    tmp = tmp + vs[0]
+            elif o == 'con':
+                for i in range(0,length):
+                    for s1 in vs[0].strings(i):
+                        for s2 in vs[1].strings(length - i):
+                            yield s1 + s2
+            elif o == 'alt':
+                for s in vs[0].strings(length):
                     yield s
+                for s in vs[1].strings(length):
+                    if not (s in vs[0]):
+                        yield s
+            elif o == 'and':
+                for s in vs[0].strings(length):
+                    if s in vs[1]:
+                        yield s
 
 emp = are.emp
 lit = are.lit
